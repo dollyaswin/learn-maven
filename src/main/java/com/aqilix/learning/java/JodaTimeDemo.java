a1b5743a6ad45ee484478dc95e815ee9dda87394
Dolly Aswin <dolly.aswin@gmail.com>
2016-08-12 10:25:16 +0700

    Add JodaTime Using Maven

---
+++ src/main/java/com/aqilix/learning/java/JodaTimeDemo.java
@@ -0,0 +1,15 @@
+package main.java.com.aqilix.learning.java;
+
+import org.joda.time.DateTime;
+
+public class JodaTimeDemo {
+
+	public static void main(String[] args) {
+		DateTime now = new DateTime();
+		DateTime nextMonth = now.plusMonths(1);
+
+		System.out.println("Next month : " + nextMonth);
+
+	}
+
+}

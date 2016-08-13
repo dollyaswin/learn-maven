35249e63035378192e564bba68bd2114d115d2b4
Dolly Aswin <dolly.aswin@gmail.com>
2016-08-13 15:52:25 +0700

    Get Total Records

--- src/main/java/com/aqilix/learning/java/ParseCSV.java
+++ src/main/java/com/aqilix/learning/java/ParseCSV.java
@@ -3,6 +3,8 @@ package com.aqilix.learning.java;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -12,15 +14,22 @@ public class ParseCSV {
 	public static void main(String[] args) {
 		String dataDir = System.getProperty("user.dir") + "/data";
 		String csvFile = dataDir + "/sample.csv";
-		CSVParser parser = null;
+		CSVParser csvParser = null;
 		Reader in = null;
 		
 		try {
 			in = new FileReader(csvFile);
-			parser = CSVFormat.EXCEL.withHeader().parse(in);
-			Iterable<CSVRecord> records = parser.getRecords();
+			// set csv format
+			CSVFormat csvFormat = CSVFormat.EXCEL.withHeader();
+			// initialize csv parser
+            csvParser = new CSVParser(in, csvFormat);
+            // get csv records
+            List<CSVRecord> csvRecords = csvParser.getRecords();
 			
-			for (CSVRecord record : records) {
+            System.out.println("Total Records: " + csvRecords.size());
+			
+            // iterate records
+			for (CSVRecord record : csvRecords) {
 				String uuid = record.get("uuid");
 				String name = record.get("name");
 				
@@ -35,7 +44,7 @@ public class ParseCSV {
 		finally {
 			try {
 				in.close();
-				parser.close();
+				csvParser.close();
 			} catch (IOException e) {
 				System.err.println("Error closing CSV resource");
 				e.printStackTrace();

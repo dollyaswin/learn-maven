e1774523ec03b9802c08d22db46bf354f1a63d05
Dolly Aswin <dolly.aswin@gmail.com>
2016-08-16 15:33:12 +0700

    Fix coding style

--- src/main/java/com/aqilix/learning/java/ParseCSV.java
+++ src/main/java/com/aqilix/learning/java/ParseCSV.java
@@ -22,13 +22,13 @@ public class ParseCSV {
 			// set csv format
 			CSVFormat csvFormat = CSVFormat.EXCEL.withHeader();
 			// initialize csv parser
-            csvParser = new CSVParser(in, csvFormat);
-            // get csv records
-            List<CSVRecord> csvRecords = csvParser.getRecords();
+			csvParser = new CSVParser(in, csvFormat);
+			// get csv records
+			List<CSVRecord> csvRecords = csvParser.getRecords();
 			
-            System.out.println("Total Records: " + csvRecords.size());
+			System.out.println("Total Records: " + csvRecords.size());
 			
-            // iterate records
+			// iterate records
 			for (CSVRecord record : csvRecords) {
 				String uuid = record.get("uuid");
 				String name = record.get("name");
@@ -39,7 +39,7 @@ public class ParseCSV {
 		} catch (Exception e) {
 			System.err.println("Error read CSV");
 			e.printStackTrace();
-	    }
+		}
 		
 		finally {
 			try {
